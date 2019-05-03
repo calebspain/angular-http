0701a596fb41f41636cce2e4766920770ad46470
Caleb Spain <crspain@uh.edu>
2019-05-03 16:19:27 -0500

    angular http example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-http';
+
+  name: string = '';
+  age: number;
+  found: boolean;
+
+  constructor(private httpClient: HttpClient) {}
+
+  onNameKeyUp(event: any): void {
+    this.name = event.target.value;
+    this.found = false;
+  }
+
+  getProfile() {
+    this.httpClient.get(`https://my-json-server.typicode.com/calebspain/json-faker-directory/profiles/?name=${this.name}`)
+    .subscribe(
+      (data: any[]) => {
+        if (data.length) {
+          this.age = data[0].age
+          this.found = true;
+        }
+      }
+    )
+  }
+
+  postProfile() {
+    this.httpClient.post(`https://my-json-server.typicode.com/calebspain/json-faker-directory/profiles/`,
+    {
+      name: 'mark',
+      age: 51
+    })
+    .subscribe(
+      (data: any) => {
+        console.log(data);
+      }
+    )
+  }
+
 }
